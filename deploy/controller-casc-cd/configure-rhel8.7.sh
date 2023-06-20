b6a95acff6b72e2c614841243ae60a1144a9e7ef
Tosin Akinosho <takinosh@redhat.com>
2023-06-20 12:00:19 -0400

    updating code

--- deploy/controller-casc-cd/configure-rhel8.7.sh
+++ deploy/controller-casc-cd/configure-rhel8.7.sh
@@ -72,7 +72,7 @@ function configure_python() {
         sudo pip3 install ansible-navigator
         sudo pip3 install firewall
         sudo pip3 install pyyaml
-        sudo pip install Jinja2
+        sudo pip3 install Jinja2
         sudo pip3 install ansible-vault
         echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.profile
         source ~/.profile
